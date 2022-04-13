01868c68cf12820171b575637dfe9ba841b443dd
Duncan Murchison <duncancmurchison@gmail.com>
2022-04-13 13:50:52 -0400

    fib

--- project_1.rb
+++ project_1.rb
@@ -32,3 +32,21 @@ robot_parts_copy[0] << "LEDs"
 # but it does
 puts "copy #{robot_parts_copy[0]}"
 p robot_parts[0] # => ["capacitors", "resistors", "inductors", "LEDs"]
+
+#Integer#fibonacci
+
+def fibonacci(n) # 3
+    return [] if n == 0
+    return [1] if n == 1
+    return [1, 1] if n == 2
+
+    result = fibonacci(n - 1)
+
+    sum = result[-1] + result[-2]
+
+    result << sum
+end
+
+def binary_search
+    
+end
